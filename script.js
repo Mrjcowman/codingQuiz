b65225d01ba88ff6ca7d5b234495c08e8ee38a7f
Jacob Cowan <mrjcowman@gmail.com>
2020-09-14 13:21:44 -0700

    Adjust timer to start right away

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let timeLeft = 21;
 const maxTime = 20;
+let timeLeft = maxTime+1;   // Offset by 1 so timer can start ticking right away
+
 let timeEl = document.querySelector("#timer");
 let timerLabel = document.querySelector("#timerBar .barLabel");
 let timerBar = document.querySelector("#timerBar .progress-bar");
@@ -7,17 +8,26 @@ let timerBar = document.querySelector("#timerBar .progress-bar");
 let percentage = 0;
 let progressBar = document.querySelector("#progressBar .progress-bar")
 
+
+
 // Countdown timer
-let timer = setInterval(()=>{
+const tickTimer = ()=>{
     timeLeft--;
     if(timeLeft<=0){
-        clearInterval(timer);
-        console.log("Time's up!");
+        timesUp();
     }
 
     timeEl.textContent = "Time: "+ (timeLeft.toString().padStart(2,'0'));
     timerLabel.textContent = timeLeft.toString().padStart(2,'0');
 
     timerBar.style.width = ((timeLeft-1)/maxTime*100)+"%"
-}, 1000);
+};
 
+const timer = setInterval(tickTimer, 1000);
+
+const timesUp = ()=>{
+    clearInterval(timer);
+    console.log("Time's up!");
+}
+
+tickTimer();
