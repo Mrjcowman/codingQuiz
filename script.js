debe59a3da70516f7586db3bb8877cc49478c0d4
Jacob Cowan <mrjcowman@gmail.com>
2020-09-15 11:31:55 -0700

    Added question loading from json file

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let timerBar = document.querySelector("#timerBar .progress-bar");
 let percentage = 0;
 let progressBar = document.querySelector("#progressBar .progress-bar")
 
+let questions = [];
+
 
 
 // Countdown timer
@@ -23,11 +25,63 @@ const tickTimer = ()=>{
     timerBar.style.width = ((timeLeft-1)/maxTime*100)+"%"
 };
 
-const timer = setInterval(tickTimer, 1000);
+// const timer = setInterval(tickTimer, 1000);
 
 const timesUp = ()=>{
     clearInterval(timer);
     console.log("Time's up!");
 }
 
-tickTimer();
+// TODO: Display question
+
+// Get questions from JSON file
+let loadQuestions = async ()=>{
+    try {
+        const response = await fetch("questions.JSON");
+        if (response.status !== 200) {
+            console.error("There was an issue loading the question data. Status Code: " + response.status);
+            return;
+        }
+
+        response.json().then((data) => {
+            console.log(data);
+            data.forEach(question => {
+                questions.push(question);
+            });
+        });
+    } catch (err) {
+        console.error("Fetch Error :-S", err);
+    }
+}
+
+// TODO: Handle start button press
+
+// TODO: Handle correct answer
+
+// TODO: Handle wrong answer
+
+// TODO: Handle initial input
+
+// TODO: Handle high scores
+
+// TODO: Handle restart
+
+// TODO: Randomize right answer position
+
+let initializeQuiz = () => {
+    document.querySelector("#startPromptDiv").style.visibility = "visible";
+    document.querySelector("#quizQuestionDiv").style.visibility = "hidden";
+    document.querySelector("#enterScoreForm").style.visibility = "hidden";
+    document.querySelector("#highScoreDiv").style.visibility = "hidden";
+
+    document.querySelector("#startButton").disabled = true;
+    timeLeft = maxTime+1;
+
+    if(questions.length==0)
+        await loadQuestions();
+    
+    document.querySelector("#startButton").disabled = false;
+    console.log("Initialized!");
+}
+
+initializeQuiz();
